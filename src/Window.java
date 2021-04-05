852927d246844b5e8a37179a95ec1433aef476ff
Emily <emberkheimer@uiowa.edu>
2021-04-05 18:50:44 -0500

    Background
    asset added
    BackgroundPanel class added

--- src/Window.java
+++ src/Window.java
@@ -1,23 +1,28 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 
 public class Window extends JFrame {
     public static int Height;
     public static int Width;
-
+    private Image image;
 
     public Window() {
-        Height = 1000;
-        Width = 700;
+        Height = 600;
+        Width = 800;
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //setContentPane(new BackgroundPanel());
         getContentPane().setLayout(new BorderLayout());
-        getContentPane().setBackground(Color.LIGHT_GRAY);
         getContentPane().setPreferredSize(new Dimension(Width, Height));
         pack();
         Game game = new Game();
         add(game);
+        BackgroundPanel backgroundPanel = new BackgroundPanel();
+        add(backgroundPanel);
         setVisible(true);
     }
+
     public static void main(String[] args) {
         Window window = new Window();
     }
